da481dacff3e3a9e338711cf428f7f065c325816
CodingGiraffe <adamthomas03@gmail.com>
2020-04-16 11:05:39 -0500

    towers of hanoi rough draft

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -19,24 +19,43 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-  // Your code here
+function movePiece(startStack, endStack) { //need to pop it from one and push it to another  
 
+let movingPiece = stacks[startStack].pop(stacks[startStack].length -1);
+stacks[endStack].push(movingPiece);
 }
 
-function isLegal() {
-  // Your code here
+function isLegal(startStack, endStack) {
+  
+  //set inputs
+  let fromStack = stacks[startStack]
+  let toStack = stacks[endStack]
 
+  //get last number from stack
+  let lastNumberFromStack = fromStack[fromStack.length-1]
+  let lastNumberToStack = toStack[toStack.length-1]
+
+  //compare last/top number from start stack to last/top number on end stack
+  if (fromStack.length > 0 && (lastNumberFromStack < lastNumberToStack || lastNumberFromStack == undefined)) {
+    movePiece(startStack,endStack);
+    return true
+  } else {
+    return false
+  }
 }
 
 function checkForWin() {
-  // Your code here
-
+  if(stacks.b.length == 4 || stacks.c == 4) {
+    return true
+  } else {
+    return false
+  }
 }
-
+ 
+                      //move from    //move to       
 function towersOfHanoi(startStack, endStack) {
-  // Your code here
-
+  isLegal(startStack, endStack);
+  checkForWin();
 }
 
 function getPrompt() {
